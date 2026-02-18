a23415e20cf8624abd9c5ce20285c45cb0c7ad6e
Ivan Lei <ilei0893@gmail.com>
2026-02-17 20:56:50 -0500

    Implement fetch to backend for index

--- frontend/src/components/Kanban.tsx
+++ frontend/src/components/Kanban.tsx
@@ -1,35 +1,35 @@
-import TaskSection from "./TaskSection";
-import CreateTaskForm from "./CreateTaskForm";
-import { useEffect, useState } from "react";
+import type { TaskData } from "../types/types"
+import { useEffect, useState } from "react"
+import TaskSection from "./TaskSection"
+import CreateTaskForm from "./CreateTaskForm"
 
 export default function Kanban() {
-  const [selectedState, setSelectedState] = useState<string | null>(null);
-  const [tasks, setTasks] = useState();
+  const [selectedState, setSelectedState] = useState<string | null>(null)
+  const [tasks, setTasks] = useState<TaskData[]>([])
 
   useEffect(() => {
-    setTasks(getTasks());
-  }, []);
+    const getTasks = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/tasks")
+        const json = await res.json()
 
-  async function getTasks() {
-    try {
-      const res = await fetch("http://localhost:3000/tasks");
-      const json = await res.json();
-
-      if (!res.ok) {
-        throw new Error("Couldn't get tasks");
+        if (!res.ok) {
+          throw new Error("Couldn't get tasks")
+        }
+        const data = json.map((task: TaskData) => ({
+          id: task.id,
+          title: task.title,
+          description: task.description,
+          state: task.state,
+          author: task.author,
+        }))
+        setTasks(data)
+      } catch (e) {
+        console.log(e)
       }
-      console.log(json);
-      return json.map((task: any) => ({
-        id: task.id,
-        title: task.title,
-        description: task.description,
-        state: task.state,
-        author: task.author,
-      }));
-    } catch (e) {
-      console.log(e);
     }
-  }
+    getTasks()
+  }, [])
 
   return (
     <main>
@@ -61,5 +61,5 @@ export default function Kanban() {
         />
       </section>
     </main>
-  );
+  )
 }
