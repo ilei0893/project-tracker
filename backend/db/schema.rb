f70e09f3d32f2fec63d120702c01647820cf1a37
Ivan Lei <ilei0893@gmail.com>
2026-02-21 00:26:22 -0500

    add User model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_17_205353) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_21_032910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,5 +21,13 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_17_205353) do
     t.string "state", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 end
