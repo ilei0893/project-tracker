1a0be95c2496eb2532071202fb4eab9567974633
Ivan <ilei0893@gmail.com>
2026-03-07 15:19:31 -0500

    split out css files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,8 @@
 import "./App.css";
+import "./styles/auth.css";
+import "./styles/nav.css";
+import "./styles/project.css";
+import "./styles/modal.css";
 import Kanban from "./components/Kanban.tsx";
 import Navbar from "./components/Navbar.tsx";
 
